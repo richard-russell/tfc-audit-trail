c7739a7e1aaf3c9171dc9ca57f49f2ed568153a7
Richard Russell <richard.russell@hashicorp.com>
2022-08-02 05:42:27 +0100

    Handle pagination using recursive function

--- tfc_get_logs.py
+++ tfc_get_logs.py
@@ -2,25 +2,35 @@ import os
 import requests
 import time
 
-POLL_INTERVAL = 10
+POLL_INTERVAL = 10  # seconds to wait between API calls
+PAGESIZE = None     # number of events to fetch with each call (None = default)
 TFC_ORG_TOKEN = os.environ.get('TFC_ORG_TOKEN')
 AUDIT_TRAIL_URL = "https://app.terraform.io/api/v2/organization/audit-trail"
 
 headers = { "Authorization": f"Bearer {TFC_ORG_TOKEN}"}
+url_params = {'since': None, 'page[size]': PAGESIZE}
 
-# r = requests.get(AUDIT_TRAIL_URL, headers = headers)
-
-url_params = {'since': None}
-
-while True:
-    r = requests.get(AUDIT_TRAIL_URL, headers=headers, params=url_params)
+def get_events(URL, headers, params, page=1):
+    """Call audit trail API and return the events.
+       Handle pagination through recursion.
+       JSON payload should contain 'events' and 'pagination' at the top level.
+    """
+    url_params['page'] = None if page == 1 else page
+    r = requests.get(URL, headers=headers, params=url_params)
     r.raise_for_status()
-
     json_response = r.json()
     events = json_response['data']
+    next_page = json_response['pagination']['next_page']
+    if not next_page:
+        return events
+    return events + get_events(URL, headers, params, page=next_page)
+
+# Main loop
+while True:
+    events = get_events(AUDIT_TRAIL_URL, headers=headers, params=url_params)
 
     if events:
-        url_params['since'] = json_response['data'][0]['timestamp']
+        url_params['since'] = events[0]['timestamp']
         for event in events:
             print(event)
 
